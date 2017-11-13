54af9c326c7232943e8276e02ae26c6bc87c27f4
Nolan Johnson <n0_j0@yahoo.com>
2017-11-13 10:38:42 -0700

    master branch

--- spark_ec2.py
+++ spark_ec2.py
@@ -113,7 +113,7 @@ DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
 DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/lingkio/spark-ec2"
-DEFAULT_SPARK_EC2_BRANCH = "branch-2.0"
+DEFAULT_SPARK_EC2_BRANCH = "master"
 
 
 def setup_external_libs(libs):
